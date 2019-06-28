c05ce8d826c66ca9f87404b388ec7c596a10e0d8
tomeklewandowski <lewytomek@gmail.com>
2019-06-28 21:20:11 +0200

    start project, made first migrations, create admin/superuser, create models for app

---
+++ ticketapp/ticket_app/models.py
@@ -0,0 +1,39 @@
+from django.core.serializers import json
+from django.db import models
+
+
+ticketsType = (
+    ("1", "Regular"),
+    ("2", "Premium"),
+    ("3", "VIP"),
+    ("4", "Reduced"),
+)
+
+reservationStatus = (
+    ("1", "Available"),
+    ("2", "Reserved"),
+    ("3", "Paid"),
+)
+
+
+class Event(models.Model):
+    id = models.AutoField(primary_key=True),
+    name = models.CharField(max_length=256),
+    date = models.DateTimeField,
+    types = models.TextField(default=None),
+
+    def set_types(self, types_list):
+        self.set_types = json.dumps(types_list)
+
+    def get_types(self):
+        return json.loads(self.set_types)
+
+
+class Ticket(models.Model):
+    id = models.AutoField(primary_key=True),
+    price = models.DecimalField(decimal_places=2),
+    ticket_type = models.IntegerField(choices=ticketsType, default=None),
+    reservation_date = models.DateTimeField(null=True),
+    reservation_status = models.IntegerField(choices=reservationStatus, default=1),
+    event = models.ForeignKey(Event),
+
